de071548858120c3b6db4af21517a7a9528b37ba
safroniaziz <safroni.aziz@gmail.com>
2021-12-15 02:31:46 +0700

    memperbaiki pdf

--- resources/views/tendik/laporan.blade.php
+++ resources/views/tendik/laporan.blade.php
@@ -12,10 +12,6 @@
     .img-top {
         opacity: 0.6;
     }
-    #header tr td {
-        border: none !important;
-        border-collapse: collapse;
-    }
 
     .page-break {
         page-break-after: always;
@@ -32,9 +28,9 @@
     }
 </style>
 <body>
-    <table id="header" cellspacing="0" cellpadding="0" style="width:100%" class="top">
+    <table id="header" cellspacing="0" cellpadding="0" style="width:100%;border: none !important;border-collapse: collapse;" class="top">
         <tr >
-            {{-- <td rowspan="5" style="width: 5% !important;" class="logo-koperasi"><img src="{{ asset('assets/images/logo.png') }}" style="width: 75px !important"></td> --}}
+            <td rowspan="5" style="width: 5% !important;" class="logo-koperasi"><img src="{{ asset('assets/images/logo.png') }}" style="width: 100px !important"></td>
             <td style="width:95% !important;" align="center"><b>KEMENTERIAN AGAMA REPUBLIK INDONESIA</b></td>
         </tr>
         <tr style="width: 100%">
@@ -53,7 +49,7 @@
     <hr style="height: 1px; background:black;">
     <div style="text-align: center">
         <h5>LAPORAN KEGIATAN HARIAN TENDIK</h5>
-        {{ $model['tanggal_awal'] }} - {{ $model['tanggal_akhir'] }}
+        {{ $model['tanggal_awal'] }} s/d {{ $model['tanggal_akhir'] }}
     </div>
 
     <table>
@@ -91,14 +87,24 @@
     </table>
     <br>
     <br>
-    <table class="table table-bordered table-hover" id="data" style="width: 100% !important; ">
+    <table class="table table-bordered table-hover" id="data" style="width: 100% !important; border-collapse: collapse; width: 100%; " >
         <thead>
-            <tr style="border:2px black solid">
-                <th>No</th>
-                <th>Hari / Tanggal</th>
-                <th>Kegiatan Tendik</th>
-                <th>Catatan Kepala Tata Usaha</th>
-                <th>Catatan Kepala Madrasah</th>
+            <tr>
+                <th style="padding: 8px !important;
+  text-align: left !important;
+  border-bottom: 1px solid #ddd !important;">No</th>
+                <th style="padding: 8px !important;
+  text-align: left !important;
+  border-bottom: 1px solid #ddd !important;">Hari / Tanggal</th>
+                <th style="padding: 8px !important;
+  text-align: left !important;
+  border-bottom: 1px solid #ddd !important;">Kegiatan Tendik</th>
+                <th style="padding: 8px !important;
+  text-align: left !important;
+  border-bottom: 1px solid #ddd !important;">Catatan Kepala Tata Usaha</th>
+                <th style="padding: 8px !important;
+  text-align: left !important;
+  border-bottom: 1px solid #ddd !important;">Catatan Kepala Madrasah</th>
             </tr>
         </thead>
         <tbody>
@@ -107,11 +113,21 @@
             @endphp
             @foreach ($laporans as $laporan)
                 <tr>
-                    <td>{{ $no++ }}</td>
-                    <td>{{ $laporan->kegTgl }}</td>    
-                    <td>{!! $laporan->kegTendik !!}</td>    
-                    <td>{!! $laporan->kegSaranKatu !!}</td>    
-                    <td>{!! $laporan->kegSaranKepsek !!}</td>    
+                    <td style="padding: 8px !important;
+  text-align: left !important;
+  border-bottom: 1px solid #ddd !important;">{{ $no++ }}</td>
+                    <td style="padding: 8px !important;
+  text-align: left !important;
+  border-bottom: 1px solid #ddd !important;">{{ $laporan->kegTgl }}</td>    
+                    <td style="padding: 8px !important;
+  text-align: left !important;
+  border-bottom: 1px solid #ddd !important;">{!! $laporan->kegTendik !!}</td>    
+                    <td style="padding: 8px !important;
+  text-align: left !important;
+  border-bottom: 1px solid #ddd !important;">{!! $laporan->kegSaranKatu !!}</td>    
+                    <td style="padding: 8px !important;
+  text-align: left !important;
+  border-bottom: 1px solid #ddd !important;">{!! $laporan->kegSaranKepsek !!}</td>    
                 </tr>
             @endforeach
         </tbody>
